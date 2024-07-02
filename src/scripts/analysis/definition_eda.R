5c7ea056f11957aa731fee05e3878672ea511a05
ASW-Analyst <andy.wilson8@nhs.net>
2024-07-02 11:52:52 +0100

    Add first episode identification

--- src/scripts/analysis/definition_eda.R
+++ src/scripts/analysis/definition_eda.R
@@ -13,3 +13,11 @@ spells_alcohol <- nrow(spells_alcohol_df)
 
 admission_date_start <- min(spells_df$Admission_Date)
 admission_date_end <- max(spells_df$Admission_Date)
+
+
+# First Episode Identified ------------------------------------------------
+
+spells_first_df <- spells_df %>%
+  filter(substr(Der_Diagnosis_All,3,6) %in% c("K740", "K760"))
+
+spells_first <- nrow(spells_first_df)         
