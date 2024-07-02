a81588fe399071f3fef2683be065459cfc4185a5
ASW-Analyst <andy.wilson8@nhs.net>
2024-07-02 09:16:12 +0100

    Create spells definition analysis script

---
+++ src/scripts/analysis/definition_eda.R
@@ -0,0 +1,15 @@
+
+# Source spells data load -------------------------------------------------
+
+source(paste0(here(), "/src/scripts/etl/definition_eda_load.R"))
+
+
+# Dataset details ---------------------------------------------------------
+
+spells_total <- nrow(spells_df)
+spells_alcohol_df <- spells_df %>%
+  filter(Alcohol_Wholly_Att_Flag == 1)
+spells_alcohol <- nrow(spells_alcohol_df)
+
+admission_date_start <- min(spells_df$Admission_Date)
+admission_date_end <- max(spells_df$Admission_Date)
