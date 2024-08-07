21123115d6a26045c43dc213731ee30fd283bda5
ASW-Analyst <andy.wilson8@nhs.net>
2024-08-07 17:01:02 +0100

    Define charts required for quarto output

---
+++ src/results/charts/eda.R
@@ -0,0 +1,5 @@
+chart_eda_ip_ts <- chart_spells_ts
+chart_eda_ip_ts_ag <- chart_spells_ts_ag
+  
+chart_eda_ip_demo_age <- chart_spells_demo_age_histo
+chart_eda_ip_demo_age_ag <- chart_spells_demo_age_ag_histo
