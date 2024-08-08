3e2db7e52cfe84da11b7cb34fdf75612dd914807
ASW-Analyst <andy.wilson8@nhs.net>
2024-08-08 10:27:26 +0100

    Create admissions group breakdown stats

--- src/scripts/analysis/eda_ip_ts.R
+++ src/scripts/analysis/eda_ip_ts.R
@@ -10,3 +10,17 @@ spells_ts_df <- spells_df_proc %>%
 spells_ts_ag_df <- spells_df_proc %>%
   group_by(admission_month, admission_group) %>%
   summarise(Total = n())
+
+
+# # Percentage of Admissions by Group -------------------------------------
+
+spells_ag <- spells_ts_ag_df %>%
+  group_by(admission_group) %>%
+  summarise(Total = sum(Total, na.rm = TRUE)) %>%
+  mutate(Prop = Total/sum(Total, na.rm = TRUE))
+
+spells_ag_recent <- spells_ts_ag_df %>%
+  filter(admission_month >= "2022-04-01") %>%
+  group_by(admission_group) %>%
+  summarise(Total = sum(Total, na.rm = TRUE)) %>%
+  mutate(Prop = Total/sum(Total, na.rm = TRUE))
