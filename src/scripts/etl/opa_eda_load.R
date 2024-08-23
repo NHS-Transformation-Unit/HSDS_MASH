44e73b78cb6f55569356d220e7e1fc032b86c717
ASW-Analyst <andy.wilson8@nhs.net>
2024-08-23 15:18:21 +0100

    Fix naming

--- src/scripts/etl/opa_eda_load.R
+++ src/scripts/etl/opa_eda_load.R
@@ -3,6 +3,6 @@
 
 source(paste0(here(), "/src/config/connection.R"))
 
-# Load Spells data --------------------------------------------------------
+# Load OPA data -----------------------------------------------------------
 
 opa_df <- DBI::dbGetQuery(con, statement = read_file(paste0(here(),"/src/scripts/etl/udal_extracts/mash_opa_analysis.sql")))
